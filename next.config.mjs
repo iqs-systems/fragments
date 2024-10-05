78a82e0fa3d5bd225b9b1c8ffa746f87c5a3c72c
IQ <19837094+iqs-systems@users.noreply.github.com>
2024-10-06 02:31:39 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-export default nextConfig
-
-module.exports = {
+const nextConfig = {
   webpack: (config) => {
     config.optimization.splitChunks = {
       chunks: 'all',
-      maxSize: 250000,  // Ограничение на размер бандлов (250 KB)
+      maxSize: 250000, // Ограничение на размер бандлов (250 KB)
     };
     return config;
   },
 };
+
+export default nextConfig;
+
